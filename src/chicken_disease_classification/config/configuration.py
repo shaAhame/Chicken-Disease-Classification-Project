792c7c6f3bdf72bbaea5808d58d1fe881b91fe11
Shakeeb Ahamed <shakeebahamed456@gmail.com>
2025-12-01 22:39:24 +0530

    prepare base model completed

--- src/chicken_disease_classification/config/configuration.py
+++ src/chicken_disease_classification/config/configuration.py
@@ -1,7 +1,15 @@
-from chicken_disease_classification.utils.common import read_yaml_file, create_directories
-from chicken_disease_classification.constants import *
-from chicken_disease_classification.entity.config_entity import DataIngestionConfig
+from chicken_disease_classification.utils.common import (
+    read_yaml_file,
+    create_directories,
+)
+from chicken_disease_classification.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
+from chicken_disease_classification.entity.config_entity import (
+    DataIngestionConfig,
+    PrepareBaseModelConfig,
+)
 from pathlib import Path
+
+
 class ConfigurationManager:
     def __init__(
         self,
@@ -23,3 +31,20 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        create_directories([Path(config.root_dir)])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES,
+        )
+
+        return prepare_base_model_config
